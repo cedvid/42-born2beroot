0fa9171687b4c284bacaca19af158d3b31dfeee8
Cédric V <101518014+cedvid@users.noreply.github.com>
2023-02-27 14:27:06 +0100

    Update script

--- monitoring.sh
+++ monitoring.sh
@@ -1,2 +1,32 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+ARC=$(uname -a)
+PPROC=$(grep 'physical id' /proc/cpuinfo | uniq | wc -l)
+VPROC=$(cat /proc/cpuinfo | grep processor | wc -l)
+RAMPC=$(echo "in progress")
+MEMPC=$(echo "in progress")
+PROCPC=$(echo "in progress")
+LASTBOOT=$(who -b | awk '{print $4, $3}')
+LVM=$(echo "in progress")
+NBRCON=$(echo "in progress")
+NBRUSER=$(who | wc -l)
+IPVMAC=$(echo "in progress")
+NBRSUDO=$(echo "in progress")
+
+wall "
+--------------------------SYSTEM INFORMATION--------------------------------
+#Architecture: $ARC 
+#CPU physical: $PPROC
+#vCPU: $VPROC
+#Memory Usage: $RAMPC
+#Disk Usage: $MEMPC
+#CPU load: $PROCPC
+#Last boot: $LASTBOOT 
+#LVM use: $LVM
+#Connections TCP:$NBRCON
+#User log: $NBRUSER
+#Network: $IPVMAC
+#Sudo: $NBRSUDO commands
+----------------------------------------------------------------------------
+"
 
